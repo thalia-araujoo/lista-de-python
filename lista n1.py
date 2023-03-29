116e2cf87b161839a692241b06d6256fe7b85b62
Liah Souza <115299056+thalia-araujoo@users.noreply.github.com>
2023-03-29 16:36:49 -0300

    Add files via upload

---
+++ lista n1.py
@@ -0,0 +1,8 @@
+print('hello word')
+nome = 'thalia'
+print(nome)
+nome = input('Digite seu nome: ')
+print('E um prazer te conhecer. Seja bem-vindo', nome)
+
+idade = input('Qual sua idade, liah?: ')
+print(idade)
